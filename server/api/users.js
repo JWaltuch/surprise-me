fa9a118e5f74f338d177a378f49140c582bfdc99
JWaltuch <jenna.waltuch@gmail.com>
2019-11-14 12:30:12 -0500

    api route gets all users

--- server/api/users.js
+++ server/api/users.js
@@ -1,16 +1,17 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const firebase = require('firebase')
+const db = firebase.database()
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
+    await db
+      .ref(`/users`)
+      .once('value')
+      .then(async function(snapshot) {
+        const users = await snapshot.val()
+        res.json(users)
+      })
   } catch (err) {
     next(err)
   }
