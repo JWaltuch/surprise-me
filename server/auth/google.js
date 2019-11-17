fbf82fd4700a03db0d2342ed423b1637fd28f534
JWaltuch <jenna.waltuch@gmail.com>
2019-11-16 20:23:15 -0500

    commented out passport bit, added firebase deployment starters

--- server/auth/google.js
+++ server/auth/google.js
@@ -1,4 +1,4 @@
-const passport = require('passport')
+// const passport = require('passport')
 const router = require('express').Router()
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 module.exports = router
